4367274ae8903d2536ceea39ba8026c01a411625
anch365 <mchindaanchoura@gmail.com>
2026-05-29 15:35:22 +0200

    La page d'accueil

--- _partials/_footer.php
+++ _partials/_footer.php
@@ -1,7 +1,14 @@
+<footer class="flex flex-col gap-6 pb-4 justify-center items-center">
+    <div class="flex flex-row gap-8">
+        <img src="../assets/imgs/mail.svg" alt="Logo de contact par mail" class="items-center">
+        <p>Contactez nous par mail</p>
+    </div>
+    <div class="flex flex-row gap-8">
+        <img src="../assets/imgs/Localisation.svg" alt="Logo de localistaion" class="items-center">
+        <p>L'adresse où nous trouver</p>
+    </div>
 
-    <footer>
-<img src="" alt="">
-    </footer>
+</footer>
 </body>
 
 </html>
