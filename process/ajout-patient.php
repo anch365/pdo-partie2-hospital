9308405b96c86595d8312d6b1fcdd5c8ac42db7e
anch365 <mchindaanchoura@gmail.com>
2026-05-30 22:59:31 +0200

    Ajout des patients dans process

--- process/ajout-patient.php
+++ process/ajout-patient.php
@@ -8,48 +8,70 @@ if ($_SERVER['REQUEST_METHOD'] !== "POST") {
 }
 
 // Deuxieme étape de sécurité : verifier que la colonne voulue existe bien
-if (!isset($_POST["prenom"]) || !isset($_POST["nom"])) {
+if (!isset($_POST["nom"]) || !isset($_POST["prenom"]) || !isset($_POST["dateNaissance"]) || !isset($_POST["telephone"]) || !isset($_POST["email"])) {
   header("Location: ../process/ajout-patient.php?error=bad-method");
   exit();
 }
 
 // Troisième étape de sécurité : verifier que la colonne voulue n'est pas vide
-if (empty($_POST["nom"]) || empty($_POST["prenom"])) {
+if (empty($_POST["nom"]) || empty($_POST["prenom"]) || empty($_POST["dateNaissance"]) || empty($_POST["telephone"]) || empty($_POST["email"])) {
   header("Location: ../process/ajout-patient.php?error=bad-method");
   exit();
 }
 
 // Quatrième étape de sécurité : on empêche l'utilisation de balise (par exemple script)
-$prenom = htmlspecialchars(strip_tags(trim($_POST["prenom"])));
 $nom = htmlspecialchars(strip_tags(trim($_POST["nom"])));
-
-
-
-
-
-
-
-
-
-
+$prenom = htmlspecialchars(strip_tags(trim($_POST["prenom"])));
+$dateNaissance = (int)$_POST["dateNaissance"];
+$telephone = htmlspecialchars(strip_tags(trim($_POST["telephone"])));
+$email = htmlspecialchars(strip_tags(trim($_POST["email"])));
 
 
 // ETAPE 2 : METTRE LES DONNEE DU PATIENT EN BDD
+require_once "../utils/db_connect.php";
 
+$request = $db->prepare("INSERT INTO patient
+(nom, prenom, dateNaissance, telephone, email)
+VALUES(?,?,?,?,?)");
 
-
-
-
-
-
-
-
-
-
-
+$request->execute([
+    $nom,
+    $prenom,
+    $dateNaissance,
+    $telephone,
+    $email
+]);
 
 // ETAPE 3 : REDIRIGER SUR UNE PAGE D'AFFICHAGE
 header("Location: ../public/liste-patient.php");
 exit();
 
 ?>
+
+    <form action="../process/ajout-patient.php" method="POST" class="flex flex-col gap-8">
+        <div>
+            <select name="genre" id="genre" class="border-2 border-solid rounded-4xl">
+                <option value="Valeur">Genre</option>
+                <option value="Mr">Mr</option>
+                <option value="Mme">Mme</option>
+            </select><br> <br>
+        </div>
+        <div>
+            <label for="prenom" class="font-bold">Nom :</label>
+            <input type="text" placeholder="Ex: Théo" id="nom" name="nom" minlength="3" maxlength="50" required class="border-solid border-2 rounded-xl">
+        </div>
+        <div>
+            <label for="nom" class="font-bold">Prénom :</label>
+            <input type="text" placeholder="Ex: Michel" id="prenom" name="prenom" minlength="3" maxlength="50" required class="border-solid border-2 rounded-xl">
+        </div>
+        <div>
+            <label for="prenom" class="font-bold">Âge :</label>
+            <input type="number" placeholder="Ex: 40" id="age" name="age" minlength="1" maxlength="3" required class="border-solid border-2 rounded-xl">
+        </div>
+        <div>
+            <label for="prenom" class="font-bold">Téléphone :</label>
+            <input type="tel" placeholder="Ex: 0640132412" id="telephone" name="telephone" minlength="2" maxlength="10" required class="border-solid border-2 rounded-xl">
+        </div>
+
+        <button type="submit" class="border-solid border rounded-xl bg-bleue-bouton">Ajouter le client</button>
+    </form>
--- public/ajout-patient.php
+++ public/ajout-patient.php
@@ -1,10 +1,10 @@
 <?php require_once "../_partials/_header.php" ?>
 
-<main class="flex flex-col gap-16">
+<main class="flex flex-col gap-16 justify-center items-center">
     <h1 class="text1-ysabeau text-4xl text-center">Formulaire d'ajout de patient</h1>
 
     <form action="../process/ajout-patient.php" method="POST" class="flex flex-col gap-8">
-         <div>
+        <div>
             <select name="genre" id="genre" class="border-2 border-solid rounded-4xl">
                 <option value="Valeur">Genre</option>
                 <option value="Mr">Mr</option>
@@ -12,19 +12,27 @@
             </select><br> <br>
         </div>
         <div>
-            <label for="nom" class="font-bold">Prénom :</label>
-            <input type="text" placeholder="Ex: Michel" id="nom" name="nom" minlength="3" maxlength="50" required class="border-solid border-2 rounded-xl">
-        </div>
-        <div>
             <label for="prenom" class="font-bold">Nom :</label>
-            <input type="text" placeholder="Ex: Théo" id="prenom" name="prenom" minlength="3" maxlength="50" required class="border-solid border-2 rounded-xl">
+            <input type="text" placeholder="Ex: Théo" id="nom" name="nom" minlength="3" maxlength="50" required class="border-solid border-2 rounded-xl">
         </div>
         <div>
-            <label for="prenom" class="font-bold">Date de naissance :</label>
-            <input type="date" placeholder="Ex: Théo" id="prenom" name="prenom" minlength="3" maxlength="50" required class="border-solid border-2 rounded-xl">
+            <label for="nom" class="font-bold">Prénom :</label>
+            <input type="text" placeholder="Ex: Michel" id="prenom" name="prenom" minlength="3" maxlength="50" required class="border-solid border-2 rounded-xl">
+        </div>
+        <div>
+            <label for="dateNaissance" class="font-bold">Date de naissance :</label>
+            <input type="date" placeholder="Ex: 40" id="dateNaissance" name="dateNaissance" minlength="1" maxlength="10" required class="border-solid border-2 rounded-xl">
+        </div>
+        <div>
+            <label for="telephone" class="font-bold">Téléphone :</label>
+            <input type="tel" placeholder="Ex: 0640132412" id="telephone" name="telephone" minlength="2" maxlength="10" required class="border-solid border-2 rounded-xl">
+        </div>
+        <div>
+            <label for="email" class="font-bold">Email :</label>
+            <input type="email" placeholder="Ex: test_test@test.test" id="email" name="email" minlength="3" maxlength="50" required class="border-solid border-2 rounded-xl">
         </div>
 
-        <button type="submit" class="border-solid border rounded-xl bg-bleue-bouton">Créer le client</button>
+        <button type="submit" class="border-solid border rounded-xl bg-bleue-bouton">Ajouter le client</button>
     </form>
 </main>
 
